2ad93a430346fcb2e1e8d2f367ab12a8d3ad187b
waljja <2576723618@qq.com>
2022-12-02 15:30:12 +0800

    first init

---
+++ src/test/java/com/example/springcloudstudy/SpringCloudStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
