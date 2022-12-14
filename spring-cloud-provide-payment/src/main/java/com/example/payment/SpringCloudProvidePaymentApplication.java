776b2b72afd8edb5711d87d4fd8ca093ef1df657
waljja <2576723618@qq.com>
2022-12-14 22:36:51 +0800

    nacos配置中心

--- spring-cloud-provide-payment/src/main/java/com/example/payment/SpringCloudProvidePaymentApplication.java
+++ spring-cloud-provide-payment/src/main/java/com/example/payment/SpringCloudProvidePaymentApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author 丁国钊
@@ -15,7 +18,17 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class SpringCloudProvidePaymentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudProvidePaymentApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringCloudProvidePaymentApplication.class, args);
+        while(true) {
+            String userName = applicationContext.getEnvironment().getProperty("user.name");
+            String userAge = applicationContext.getEnvironment().getProperty("user.age");
+            System.err.println("user name :" + userName + "; age: " + userAge);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (Exception e) {
+                System.out.println("configException: " + e);
+            }
+        }
     }
 
 }
